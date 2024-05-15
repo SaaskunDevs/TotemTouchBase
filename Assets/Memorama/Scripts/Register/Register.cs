441dae794d5d3332d4638a1a6aa96400d7f0d11d
SaaskunDevs <saaskundev@gmail.com>
2024-05-15 17:50:56 -0600

    Se realizo un ajuste al Script Register para un mayor control de la escena y de los datos a enviar

--- Assets/Memorama/Scripts/Register/Register.cs
+++ Assets/Memorama/Scripts/Register/Register.cs
@@ -23,13 +23,27 @@ public class Register : MonoBehaviour
 
     [Header("Bool")]
     public bool registerExist;
+    bool inputNull = true;
 
-    
+    [Header("Array")]
+    TMP_InputField[] inputFields;
 
     
     void Start()
     {
-        
+        if(registerExist)
+        {
+            inputFields = new TMP_InputField[] {nameInputField, emailInputField, phoneInputField};
+
+            foreach(TMP_InputField inputField in inputFields)
+            {
+                if(inputField != null)
+                {
+                    inputNull = false;
+                    break;
+                }
+            }
+        }
     }
     public void CheckRegister()
     {
@@ -39,27 +53,30 @@ public class Register : MonoBehaviour
             return;
         }
 
-        //Si la variable esta declarada se le asigna el valor del inputField, si no se le asigna null
-        name = nameInputField ? nameInputField.text : null;
-        email = emailInputField ? emailInputField.text : null;
-        phone = phoneInputField ? phoneInputField.text : null;
+        if(!inputNull)
+        {
+            //Si la variable esta declarada se le asigna el valor del inputField, si no se le asigna null
+            name = nameInputField ? nameInputField.text : null;
+            email = emailInputField ? emailInputField.text : null;
+            phone = phoneInputField ? phoneInputField.text : null;
 
-        Debug.Log("Checando...");
+            Debug.Log("Checando...");
 
-        if(nameInputField && string.IsNullOrEmpty(name))
-            return;
-        if(emailInputField && (string.IsNullOrEmpty(email) || !(email.Contains("@") && (email.Contains("gmail.com") || email.Contains("hotmail.com")))))
-            return;
-        if(phoneInputField && (string.IsNullOrEmpty(phone) || phone.Length != 10))
-            return;
+            if(nameInputField && string.IsNullOrEmpty(name))
+                return;
+            if(emailInputField && (string.IsNullOrEmpty(email) || !(email.Contains("@") && (email.Contains("gmail.com") || email.Contains("hotmail.com")))))
+                return;
+            if(phoneInputField && (string.IsNullOrEmpty(phone) || phone.Length != 10))
+                return;
 
-        //Siempre tener la instancia de la clase UsersLoad para poder guardar los datos
-        Debug.Log("Datos correctos");
-        UsersLoad user = main.UserDataInstance;
-        user.name = name;
-        user.email = email;
-        user.phoneNumber = int.TryParse(phone, out int phoneNumber) ? phoneNumber : 0;
-        
-        uiManager.GoGame();
+            //Siempre tener la instancia de la clase UsersLoad para poder guardar los datos
+            Debug.Log("Datos correctos");
+            UsersLoad user = main.UserDataInstance;
+            user.name = name;
+            user.email = email;
+            user.phoneNumber = int.TryParse(phone, out int phoneNumber) ? phoneNumber : 0;
+            
+            uiManager.GoGame();
+        }
     }
 }
